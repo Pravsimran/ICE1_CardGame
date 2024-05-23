cbaa8111942ac871dc87b23a2c62f5cca6ac0b04
Pravsimran <kaupravs@sheridancollege.ca>
2024-05-22 20:31:24 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -1,32 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package card;
-
-/**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
- */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+    private Card[] hand = new Card[7];
+    private String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+    public CardTrick() {
+        for (int i = 0; i < 7; i++) {
+            int cardNumber = (int) (Math.random() * 13) + 1;
+            String suit = suits[(int) (Math.random() * 4)];
+            hand[i] = new Card(cardNumber, suit);
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
     }
-    
+
+    public void play() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Pick a card (any card): ");
+        int cardNumber = scanner.nextInt();
+        String suit = scanner.next();
+
+        boolean found = false;
+        for (Card card : hand) {
+            if (card.getNumber() == cardNumber && card.getSuit().equals(suit)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Congratulations, your card is in the magic hand!");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
+    }
+
+    public static void main(String[] args) {
+        CardTrick cardTrick = new CardTrick();
+        cardTrick.play();
+    }
 }
